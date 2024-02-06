cf975f1ef28dcfaee7ed9ffebe949cda16ce4a43
Tarun Singh <tarun.singh1120@gmail.com>
2024-02-07 00:13:05 +0530

    vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { inject } from '@vercel/analytics';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+inject();
